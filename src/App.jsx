de0681a1e9a194faa5014f3e658347a8a693168d
dangminh214 <dangminh214@gmail.com>
2024-06-01 19:53:53 +0200

    Add change and state between add project and no projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState, selectedProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    selectedProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+  
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar />
-      <NewProject />
+      <ProjectSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
